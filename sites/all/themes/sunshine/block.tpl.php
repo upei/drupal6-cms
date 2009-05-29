2b15dc6f3c26609cb2ca2318233f5bd28d048e64
Drupal <drupal@chinstrap.cs.upei.ca>
2009-05-29 16:19:26 -0300

    [add] block.tpl.php to add pathfilter support [new] pathfilter module]

--- sites/all/themes/sunshine/block.tpl.php
+++ sites/all/themes/sunshine/block.tpl.php
@@ -1,4 +1,11 @@
 <div class="block block-<?php print $block->module; ?>" id="block-<?php print $block->module; ?>-<?php print $block->delta; ?>">
-  <?php if ($block->subject) { ?><h2 class="title"><?php print $block->subject; ?></h2><?php } ?>
+<?php if ($block->subject) { ?><h2 class="title"><?php
+if (module_hook('pathfilter','filter')) {
+  print module_invoke('pathfilter', 'filter', 'process', 0, -1, html_entity_decode($block->subject));
+}
+else {
+  print html_entity_decode($block->subject);
+}
+?></h2><?php } ?>
   <?php if ($block->content) { ?><div class="content"><?php print $block->content; ?></div><?php } ?>
 </div>
