941ebed9c2bc8a954594a2d3c797dd5cf0190da0
Drupal <drupal@chinstrap.cs.upei.ca>
2009-09-21 10:27:15 -0300

    * update

---
+++ sites/all/themes/sunshine/node-media.tpl.php
@@ -0,0 +1,63 @@
+<div class="node-media node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>">
+  <div class="left">
+    <div class="field field-field-image"><?= $field_image[0]['view']?></div>
+    <div class="field field-field-image-data-title"><?= $field_image[0]['data']['title']?></div>
+    <?php if ($field_pull_quote[0]['view']): ?>
+    <div class="field field-field-pull-quote"><?= $field_pull_quote[0]['view']?></div>
+    <?php endif; ?>
+  </div>
+  <div class="right">
+    <fieldset>
+      <legend>Useful Links</legend>
+      <div class="links">
+        <ul class="links">
+          <li class="first last">
+          <a href="<?=base_path()?>media">Return to Media Releases</a>
+          </li>
+        </ul>
+      </div>
+    <?php if ($links): ?>
+      <div class="links"><?php print $links; ?></div>
+    <?php endif; ?>
+    </fieldset>
+    <fieldset>
+      <legend>Contact Information</legend>
+      <div class="field field-field-contact-name">
+        <span class="label">Contact Person:</span>
+        <span class="value"><?=$field_contact_name[0]['view']?></span>
+      </div>
+      <div class="field field-field-department">
+        <span class="label">Department:</span>
+        <span class="value"><?=$field_department[0]['view']?></span>
+      </div>
+      <div class="field field-field-phone">
+        <span class="label">Phone:</span>
+        <span class="value"><?=$field_phone[0]['view']?></span>
+      </div>
+      <?php if ($field_email[0]): ?>
+      <div class="field field-field-email">
+        <span class="label">Email:</span>
+        <span class="value"><?= $field_email[0]['safe']?></span>
+      </div>
+      <?php endif; ?>
+    </fieldset>
+  </div>
+  <div class="content">
+    <? if ($page == 0): ?>
+    <h2 class="title"><a href="<?=$node_url?>"><?=$title?></a></h2>
+    <? endif; ?>
+    <div class="date">Posted on <?php print date('l, j F Y', $created); ?></div>
+    <div class="field field-field-image"><?= $field_image[1]['view'] ?></div>
+    <div class="field field-field-image-data-title"><?= $field_image[1]['data']['title'] ?></div>
+    <div class="body">
+    <?php
+      // filter out unneccessary p tags
+      print preg_replace(
+        array("/<p>(&nbsp;| )*<\/p>/"),
+        array(''),
+        $body); ?>
+    </div>
+  </div>
+  <div class="clear-block clear"></div>
+
+</div>
