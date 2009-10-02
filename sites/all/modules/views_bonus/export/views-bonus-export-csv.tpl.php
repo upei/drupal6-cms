60eb8f0496f348f7229dc2979e8b38f602470068
Drupal <drupal@chinstrap.cs.upei.ca>
2009-10-02 10:49:04 -0300

    * update template

--- sites/all/modules/views_bonus/export/views-bonus-export-csv.tpl.php
+++ sites/all/modules/views_bonus/export/views-bonus-export-csv.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: views-bonus-export-csv.tpl.php,v 1.3 2008/12/09 17:02:41 neclimdul Exp $
+// $Id: views-bonus-export-csv.tpl.php,v 1.5 2009/05/09 18:44:31 neclimdul Exp $
 /**
  * @file views-view-table.tpl.php
  * Template to display a view as a table.
@@ -17,16 +17,10 @@
 
 // Print out header row, if option was selected.
 if ($options['header']) {
-  if ($options['quote']) echo '"';
-  print implode($seperator, $header);
-  if ($options['quote']) echo '"';
-  echo "\r\n";
+  print implode($seperator, $header) . "\r\n";
 }
 
 // Print out exported items.
 foreach ($themed_rows as $count => $item_row):
-  if ($options['quote']) echo '"';
-  print implode($seperator, $item_row);
-  if ($options['quote']) echo '"';
-  echo "\r\n";
+  print implode($seperator, $item_row) . "\r\n";
 endforeach;
--- sites/all/modules/views_bonus/export/views-bonus-export-xml.tpl.php
+++ sites/all/modules/views_bonus/export/views-bonus-export-xml.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: views-bonus-export-xml.tpl.php,v 1.1 2008/10/28 02:18:32 neclimdul Exp $
+// $Id: views-bonus-export-xml.tpl.php,v 1.2 2009/06/24 17:27:53 neclimdul Exp $
 /**
  * @file views-view-table.tpl.php
  * Template to display a view as a table.
@@ -7,7 +7,7 @@
  * - $title : The title of this group of rows.  May be empty.
  * - $rows: An array of row items. Each row is an array of content
  *   keyed by field ID.
- * - $header: an array of haeaders(labels) for fields.
+ * - $header: an array of headers(labels) for fields.
  * - $themed_rows: a array of rows with themed fields.
  * @ingroup views_templates
  */
@@ -21,7 +21,7 @@ print '<?xml version="1.0" encoding="UTF-8" ?>';
 <?php foreach ($row as $field => $content):
     $label = $header[$field] ? $header[$field] : $field;
 ?>
-    <<?php print $label; ?>><![CDATA[<?php print $content; ?>]]></<?php print $label; ?>>
+    <<?php print $label; ?>><?php print $content; ?></<?php print $label; ?>>
 <?php endforeach; ?>
   </node>
 <?php endforeach; ?>
