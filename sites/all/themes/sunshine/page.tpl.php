10b8dbc39140e59bf001b3dae866e924cfd23fbb
Drupal <drupal@chinstrap.cs.upei.ca>
2009-10-19 10:32:50 -0300

    * move page classes definition to preprocess function

--- sites/all/themes/sunshine/page.tpl.php
+++ sites/all/themes/sunshine/page.tpl.php
@@ -27,7 +27,7 @@
   <div style="clear:both"></div>
 </div>
 <?php include 'site-includes/02.100-suckerfish.php'; ?>
-<div id="page" class="<?=$is_front ? "page-front" : ""?><?= $node ?" page-$node->type" : ""?><?= $node ? " page-nid-$node->nid" : ""?>">
+<div id="page" class="<?php print $page_classes ?>">
   <?php include 'site-includes/02-page.tpl.php'; ?>
   <div style="clear:both"></div>
 </div>
