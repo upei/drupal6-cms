7e9fc77c40b806b481632f7a3497c8c858f9fad6
Drupal <drupal@chinstrap.cs.upei.ca>
2009-11-04 14:34:29 -0400

    * fix simplenews * bottom links

--- sites/all/themes/sunshine_less/site-includes/03-footer.php
+++ sites/all/themes/sunshine_less/site-includes/03-footer.php
@@ -1,6 +1,8 @@
+<?php if ($links1 || $links2 || $links3 || $links4 || $links5 || $links6): ?>
 <div class="clear-block clr" id="links-section">                                                                        
   <?php include '02.420-bottom-links.php'; ?>
 </div><!-- /bottom links -->
+<?php endif; ?>
 
 <?php if ($footer) { ?>
   <div id="footer-region"><?php print $footer?></div>
